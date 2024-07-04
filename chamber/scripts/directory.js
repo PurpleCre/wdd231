a7f2a3b495796af252833d3457301827144a3754
PurpleCre <pgonese6@gmail.com>
2024-07-04 13:02:28 +0200

    chamber: init

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,26 @@
+const cards = document.querySelector("#buss");
+const ham = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+// responsive ham and nav
+ham.addEventListener("click", () => {
+  ham.classList.toggle("show");
+  nav.classList.toggle("show");
+});
+
+// grid/list toggle
+gridbutton.addEventListener("click", () => {
+  cards.classList.add("grid");
+  cards.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+  cards.classList.add("list");
+  cards.classList.remove("grid");
+});
+
 // Instatiate date object
 const date = new Date();
 
@@ -7,41 +30,34 @@ document.querySelector(
   "#lastmodified"
 ).innerHTML = `Last modified: ${document.lastModified}`;
 
-const url = "/data/members.json"
+const url = "./data/members.json";
 
-async function getProphetData(url) {
+async function getMemberData(url) {
   const response = await fetch(url);
 
   const data = await response.json();
   console.table(data);
-  displayProphets(data.prophets);
+  cardsMembers(data);
 }
 
-const displayProphets = (prophets) => {
-  prophets.forEach((prophet) => {
-    // Create elements to add to the div.cards element
+const cardsMembers = (members) => {
+  members.forEach((member) => {
+    // Create elements to add to the bussiness section
     let card = document.createElement("section");
-    let fullName = document.createElement("h2"); // fill in the blank
-    let portrait = document.createElement("img");
 
-    // Build the h2 content out to show the prophet's full name
-    fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
-    // Build the image portrait by setting all the relevant attributes
-    portrait.setAttribute("src", prophet.imageurl);
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname}`
-    ); // fill in the blank
-    portrait.setAttribute("loading", "lazy");
-    portrait.setAttribute("width", "340");
-    portrait.setAttribute("height", "440");
-
-    // Append the section(card) with the created elements
-    card.appendChild(fullName); //fill in the blank
-    card.appendChild(portrait);
+    card.innerHTML = `<h3>${member.name}</h3>
+    <p>${member.motto}</p>
+    <div class="stuff">
+      <img src="${member.image}" alt="${member.name} businness image" />
+      <div>
+        <p><strong>Email:</strong> ${member.mail}</p>
+        <p><strong>Phone:</strong> ${member.number}</p>
+        <p><strong>URL:</strong> ${member.url}</p>
+      </div>
+    </div>`;
 
     cards.appendChild(card);
   });
 };
 
-getProphetData(url);
+getMemberData(url);
